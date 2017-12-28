558b0f6aa5d1a960ebc047085e12095230fb00e1
Marcin <basistamarcin@gmail.com>
2017-12-28 16:08:11 +0100

    Added service to get audio volume

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 
 import subprocess
+import re
 
 app = Flask(__name__)
 
@@ -10,8 +11,16 @@ app = Flask(__name__)
 def set_audio_volume():
     volume = request.args.get('volume')
     bash_command = "amixer -D pulse sset Master " + volume + "%"
-    process = subprocess.Popen(bash_command.split())
-    output,error = process.communicate()
+    subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    return volume
+
+
+@app.route('/remote', methods=['GET'])
+def get_audio_volume():
+    bash_command = "amixer -D pulse get Master"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    amixer_stdout = process.communicate()[0]
+    volume = re.search('\[([0-9]*)%\]', amixer_stdout).group(1)
     return volume
 
 
